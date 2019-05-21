fecb8fb109e1583049f52d61fc2d369d5a9269a8
Аслан Нурсеитов <grooves97@gmail.com>
2019-05-21 10:39:46 +0600

    Анимировал картинку и разрисовал картинку два проекта WPF

---
+++ Painting/Animation/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Animation
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = ellipse.Width;
+            doubleAnimation.To = ellipse.Width + 300;
+            doubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(5));
+            doubleAnimation.AutoReverse = true;
+            doubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
+
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(doubleAnimation);
+            Storyboard.SetTargetName(doubleAnimation, ellipse.Name);
+            Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath(WidthProperty));
+
+            storyboard.Begin(this);
+        }
+    }
+}
